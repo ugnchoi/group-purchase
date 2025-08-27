299b32236aee28e25f0d7edebeadb7e5bd3d2f95
ugnchoi <ugn.choi@gmail.com>
2025-08-28 00:21:47 +0900

    Remove i18n configuration from sitemap to fix API redirects

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,16 +3,4 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
-  alternateRefs: [
-    {
-      href: "http://localhost:3000/ko",
-      hreflang: "ko",
-    },
-    {
-      href: "http://localhost:3000/en",
-      hreflang: "en",
-    },
-  ],
-  defaultLocale: "ko",
-  locales: ["ko", "en"],
 };
